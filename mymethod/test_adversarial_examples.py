8955733b4d063cab3a835c7830572744866dbafd
wxz <876688461@qq.com>
2019-05-30 10:17:16 +0800

    save samples

--- mymethod/test_adversarial_examples.py
+++ mymethod/test_adversarial_examples.py
@@ -6,6 +6,7 @@ import models
 from models import MNIST_target_net
 import numpy as np
 import time
+from torch.utils.data import TensorDataset
 
 use_cuda=True
 image_nc=1
@@ -68,7 +69,29 @@ for i, data in enumerate(test_dataloader, 0):
 
 end_time = time.time()
 print("generating adversary time: {}".format(end_time-start_time))
-
 print('num_correct: ', num_correct.item())
 print('accuracy of adv imgs in testing set: %f\n'%(num_correct.item()/len(mnist_dataset_test)))
 
+
+# my adv examples
+mifgsm_b_adv = np.load('mifgsm_b_adv.npy')
+my_label = mnist_dataset.train_labels.clone()
+mydataset = TensorDataset(torch.tensor(mifgsm_b_adv),my_label)
+my_dataloader = DataLoader(mydataset,batch_size=batch_size, shuffle=False, num_workers=1)
+num_correct = 0
+for i, data in enumerate(test_dataloader, 0):
+    test_img, test_label = data
+    test_img, test_label = test_img.to(device), test_label.to(device)
+    perturbation = pretrained_G(test_img)
+    perturbation = torch.clamp(perturbation, -0.2, 0.2)
+    adv_img = perturbation + test_img
+    adv_img = torch.clamp(adv_img, 0, 1)
+    tmp = adv_img.detach().cpu().numpy()
+    pred_lab = torch.argmax(target_model(adv_img),1)
+    num_correct += torch.sum(pred_lab==test_label,0)
+
+end_time = time.time()
+print("generating adversary time: {}".format(end_time-start_time))
+
+print('num_correct: ', num_correct.item())
+print('accuracy of adv imgs in my adv: %f\n'%(num_correct.item()/len(mnist_dataset_test)))
